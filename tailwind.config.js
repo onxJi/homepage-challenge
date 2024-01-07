9bc1683b7c0850d61fdf0bcb74bd47de2e5ad8ee
onxJi <jaaziel-jz77@outlook.com>
2024-01-06 18:12:06 -0700

    actualizacion

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,7 @@ module.exports = {
   darkMode: "class",
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {
-      backgroundImage: {
-        "open-menu": "url('assets/open-menu.svg')",
-        "close-menu": "url('assets/close_menu.svg')",
-        "open-menu-dark": "url('assets/open-menu-dark.svg')",
-        "close-menu-dark": "url('assets/close_menu_dark.svg')",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
