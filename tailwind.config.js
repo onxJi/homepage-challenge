86148a5b05f72cf57147dbff01086f2821c949e1
onxJi <jaaziel-jz77@outlook.com>
2024-01-06 17:00:37 -0700

    terminando challenge

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
   darkMode: "class",
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "open-menu": "url('/assets/open-menu.svg')",
+        "close-menu": "url('/assets/close_menu.svg')",
+        "open-menu-dark": "url('/assets/open-menu-dark.svg')",
+        "close-menu-dark": "url('/assets/close_menu_dark.svg')",
+      },
+    },
   },
   plugins: [],
 };
